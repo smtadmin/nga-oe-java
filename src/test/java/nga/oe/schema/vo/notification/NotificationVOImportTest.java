0a79ab76b16be77311949b3468553ee437f4337d
William R Larsen Jr <billy@siliconmtn.com>
2022-09-23 16:30:58 -0400

    ADded Test

---
+++ src/test/java/nga/oe/schema/vo/notification/NotificationVOImportTest.java
@@ -0,0 +1,13 @@
+package nga.oe.schema.vo.notification;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+class NotificationVOImportTest {
+
+	@Test
+	void notNullTest() {
+		assertNotNull(new NotificationVOImport());
+	}
+}
